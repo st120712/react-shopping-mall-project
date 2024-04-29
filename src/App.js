e99e5242a90f0a8ff42380482e470adb21fd7ae5
100Seongjun <qortjdwns120712@gmail.com>
2024-04-29 22:34:38 +0900

    url params

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import bg from './img/bg.jpg';
 import { useState } from 'react';
 import data from './data.js';
-import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 import Detail from './routes/detail.js';
 
 function App() {
-
   let [shoes] = useState(data);
   let navigate = useNavigate();
 
@@ -15,48 +14,75 @@ function App() {
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">DHMall</Navbar.Brand>
+          <Navbar.Brand>DHMall</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
-            <Nav.Link onClick={()=>{navigate('/detail')}}>Cart</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate('/');
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate('/detail');
+              }}
+            >
+              Cart
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-
-
       <Routes>
-        <Route path='/' element={
-          <>
-            <div className='main-bg' style={{ backgroundImage: 'url(' + bg + ')' }}></div>
-            <div className='container'>
-              <div className='row'>
-                {
-                  shoes.map((data, i) => (
+        <Route
+          path="/"
+          element={
+            <>
+              <div
+                className="main-bg"
+                style={{ backgroundImage: 'url(' + bg + ')' }}
+              ></div>
+              <div className="container">
+                <div className="row">
+                  {shoes.map((data, i) => (
                     <Card data={data}></Card>
-                  ))
-                }
+                  ))}
+                </div>
               </div>
-            </div>
-          </>
-        } />
+            </>
+          }
+        />
 
-        <Route path='/detail' element={<Detail/>} />
-        <Route path='*' element={<div>없는페이지요</div>} />
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+
+        <Route path="/event" element={<Event></Event>}>
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>} />
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
+        </Route>
+        <Route path="*" element={<div>없는페이지요</div>} />
       </Routes>
+    </div>
+  );
+}
 
+function Event() {
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
     </div>
   );
 }
 
 function Card(props) {
   return (
-    <div className='col-md-4'>
+    <div className="col-md-4">
       <img src={props.data.img} width="80%" />
       <h4>{props.data.title}</h4>
-      <p>{props.data.price}</p>
+      <p>{props.data.price}원</p>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,19 +1,28 @@
-function Detail() {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6">
-                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-                </div>
-                <div className="col-md-6">
-                    <h4 className="pt-5">상품명</h4>
-                    <p>상품설명</p>
-                    <p>120000원</p>
-                    <button className="btn btn-danger">주문하기</button>
-                </div>
-            </div>
+import { useParams } from 'react-router-dom';
+
+function Detail(props) {
+  let { id } = useParams();
+
+  var shoes = props.shoes.find((o) => o.id == id);
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            width="100%"
+          />
         </div>
-    )
+        <div className="col-md-6">
+          <h4 className="pt-5">{shoes.title}</h4>
+          <p>{shoes.content}</p>
+          <p>{shoes.price}원</p>
+          <button className="btn btn-danger">주문하기</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Detail;
