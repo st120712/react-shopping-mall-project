55579f6631e5fccdb2a701469547a2cf751a6004
zzw123@naver.com <ewdongho@gmail.com>
2024-05-06 18:00:23 +0900

    최근본상품

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import bg from './img/bg.jpg';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import data from './data.js';
 import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
@@ -10,6 +10,13 @@ import Detail from './routes/Detail.js';
 
 
 function App() {
+
+  let [watched, setWatched] = useState([...JSON.parse(localStorage.getItem('watched'))]);
+
+  useEffect(() => {
+    localStorage.setItem('watched', JSON.stringify(watched));
+  }, [watched])
+
 
   let [shoes, setShoes] = useState(data);
   let navigate = useNavigate();
@@ -24,10 +31,8 @@ function App() {
         .then((result) => {
           var tmp = [...shoes];
           result.data.map((m) => {
-            console.log(m);
             m.img = `https://codingapple1.github.io/shop/shoes${m.id + 1}.jpg`;
             tmp.push(m);
-            return m;
           });
 
           setShoes(tmp);
@@ -68,11 +73,20 @@ function App() {
               <div
                 className="main-bg"
                 style={{ backgroundImage: 'url(' + bg + ')' }}
-              ></div>
+              >
+                <div className='watched'>
+                  최근본상품
+                  {
+                    watched.map((e, i) => {
+                      return <div>{e}</div>
+                    })
+                  }
+                </div>
+              </div>
               <div className="container">
                 <div className="row">
                   {shoes.map((data, i) => {
-                    return (< Card key={i} data={data} ></Card>)
+                    return (< Card key={i} data={data} watched={watched} setWatched={setWatched} ></Card>)
                   })}
                 </div>
               </div>
@@ -96,7 +110,13 @@ function App() {
 const Card = (props) => {
   return (
     <div className="col-md-4">
-      <Link to={'detail/' + props.data.id}>
+      <Link to={'detail/' + props.data.id} onClick={() => {
+        let tmp = [...props.watched];
+        tmp.unshift(props.data.title);
+        let set = new Set(tmp);
+        tmp = [...set];
+        props.setWatched(tmp);
+      }}>
         <img alt="이미지없음" src={props.data.img} width="80%" />
         <h4>{props.data.title}</h4>
         <p>{props.data.price}원</p>
